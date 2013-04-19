81f375969e2433dba6e1cc06a032eaaae9cd0c0d
unknown <dcausevic@OFFPCSA03.voc.local>
2013-04-19 11:11:36 +0200

    Popravak

---
+++ MapEditor/MapEditor/TransformBmp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace MapEditor
+{
+    class TransformBmp
+    {
+        //radi testirano donekle, rotira oko centra
+        public static Bitmap rotateBitmap(Bitmap source, float angle)
+        {
+            var newBmp = new Bitmap(source.Width, source.Height);
+            var graphics = Graphics.FromImage((Image)newBmp);
+
+            graphics.TranslateTransform((float)source.Width / 2, (float)source.Height / 2);
+            graphics.RotateTransform(angle);
+            graphics.TranslateTransform(-(float)source.Width / 2, -(float)source.Height / 2);
+            graphics.DrawImage((Image)source, new Point(0, 0));
+            return newBmp;
+        }//end method
+
+        //rotira oko (cx, cy) tacke
+        public static Bitmap rotateBitmap(Bitmap source, float angle, int cx, int cy)
+        {
+            var newBmp = new Bitmap(source.Width, source.Height);
+            var graphics = Graphics.FromImage((Image)newBmp);
+
+            graphics.TranslateTransform((float)cx / 2, (float)cy / 2);
+            graphics.RotateTransform(angle);
+            graphics.TranslateTransform(-(float)cx / 2, -(float)cy / 2);
+            graphics.DrawImage((Image)source, new Point(0, 0));
+            return newBmp;
+        }//end method
+
+    }//end class
+
+}//end namespace
